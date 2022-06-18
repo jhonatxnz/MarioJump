ebbd20dddd59672b13fb9374c8c34efcf1325a69
Jhon <jw.jhonatan1711@gmail.com>
2022-06-17 23:23:26 -0300

    boss

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const pipe = document.querySelector('.pipe')
 const score = document.querySelector('.score')
 const scoree = document.querySelector('.scoree')
 const message = document.querySelector('.message')
+const boss =  document.querySelector('.spaceship')
+const ballLaser =  document.querySelector('.ballLaser')
+const laser =  document.querySelector('.laser')
+
 
 
 const jump = () => {
@@ -36,21 +40,77 @@ const pontos = setInterval(function () {
         screen.style.background = 'linear-gradient(#87CEEB,#E0F6FF)'
     }
 
+    if(x > 900 && x < 986){
+        boss.style.animation= 'boss-animation 2s linear'
+        boss.style.visibility = 'inherit'
+        
+        if(x > 920){
+            ballLaser.style.animation= 'ballLaser-animation 2s linear'
+            ballLaser.style.visibility = 'inherit'
+            
+            pipe.style.animation = 'none';
+            pipe.style.right = '-80px'
+        }
+        if(x > 945){
+            ballLaser.style.visibility = 'hidden'
+            
+            laser.style.visibility = 'inherit'
+            laser.style.width = '1700px'
+        }
+        if(x > 965){
+            laser.style.animation= 'laser-animation 2s linear'
+        }
+        if(x > 967){
+            boss.style.animation= 'boss-animationPt2 2s linear'
+        }
+    }
+    else{
+        boss.style.visibility = 'hidden'
+        
+        laser.style.visibility = 'hidden'
+        laser.style.width = '1px'
+    
+        pipe.style.animation = 'pipe-animation 1.5s infinite linear'
+    }
+    
+
 }, 100);
 
 
 const loop = setInterval(() => {
+    
+    //console.log('loop')
 
-
-
-    console.log('loop')
-
+    const laserPosition = laser.offsetLeft;
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-    console.log(pipePosition)
+    //console.log(pipePosition)
+    //console.log(laserPosition)
+    //console.log(marioPosition)
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        score.style.animation = 'none';
+        score.style.color = 'red';
+
+        message.style.visibility = 'inherit'
+
+        clearInterval(loop);
+        clearInterval(pontos)
+    }
+    // lógica meio burra 
+    if(laserPosition < 0 && marioPosition > 140 ){
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
@@ -77,3 +137,9 @@ const loop = setInterval(() => {
 document.addEventListener('keydown', jump);
 
 
+// ESTACOES DO ANO
+//VITORIA
+//TABELA
+//GUARDAR SCORES
+//MUSICA
+//AUMENTAR VELOCIDADE
